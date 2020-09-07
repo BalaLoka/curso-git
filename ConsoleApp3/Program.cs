751208fb0ec6a4e4ec6812cbf7fb4f6f0d996f77
Willian Castro <Willianlapol@gmail.com>
2020-09-07 09:43:53 -0300

    Acrescentado bom dia para quem leu

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp3
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hy!");
+            Console.WriteLine("bom dia para quem leu");
         }
     }
 }
